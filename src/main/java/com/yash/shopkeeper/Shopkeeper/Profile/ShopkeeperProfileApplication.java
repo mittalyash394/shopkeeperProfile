1be9fc5cb82081c6463650865d29939624df2b78
mittalyash394 <mittalyash394@gmail.com>
2023-08-28 16:29:21 +0530

    added swagger to the APIs

--- src/main/java/com/yash/shopkeeper/Shopkeeper/Profile/ShopkeeperProfileApplication.java
+++ src/main/java/com/yash/shopkeeper/Shopkeeper/Profile/ShopkeeperProfileApplication.java
@@ -2,12 +2,23 @@ package com.yash.shopkeeper.Shopkeeper.Profile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class ShopkeeperProfileApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShopkeeperProfileApplication.class, args);
 	}
 
+	public Docket apis(){
+
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.yash.shopkeeper.Shopkeeper.Profile")).build();
+
+	}
 }
