5926bf9eeda6a30c3cc9624b18169c0aec849f32
mittalyash394 <mittalyash394@gmail.com>
2023-09-09 17:41:56 +0530

    added the registration, getById, getAll the shopkeeper profiles APIs without exceptions handling

---
+++ src/main/java/com/yash/shopkeeper/Shopkeeper/Profile/entity/ShopkeeperProfile.java
@@ -0,0 +1,24 @@
+package com.yash.shopkeeper.Shopkeeper.Profile.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document
+public class ShopkeeperProfile {
+
+    @Id
+    private String shopKeeperId;
+    private String shopKeeperFirstName;
+    private String shopKeeperLastName;
+    private String emailId;
+    private String password;
+    private String confirmPassword;
+    private String occupationType;
+
+}
